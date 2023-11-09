e093b08094f8a02c70febd5e08b1234b90ce4f09
workoss <workoss@icloud.com>
2023-11-09 23:59:51 +0800

    feature(docs): change to vuepress

--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -1,6 +1,6 @@
 import { alias } from '../../alias'
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default defineNuxtConfig ({
   devtools: { enabled: true },
   modules: [
     "@pinia/nuxt",
